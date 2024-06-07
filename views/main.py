899dd1d310a83ac50c1514d1040b024875cdef02
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-06-07 01:28:38 +0100

    main.py and places.py set up

--- views/main.py
+++ views/main.py
@@ -1,11 +1,18 @@
-import pandas as pd
-from setup_google_sheets import get_google_sheet
+import argparse
+from places import PlacesToVisit
 
-class PlacesToVisit:
-    def __init__(self)
-        
-        self.sheet = get_google_sheet()
-        self.places = self.load_places_from_sheet()
-        
-    def load_places_from_sheet(self):
-        data = self.sheet.get_all
+def main():
+    parser = argparse.ArgumentParser(destination="places to Visit Manager")
+    parser.add_argument('operation', choices=['add', 'remove', 'update', 'list', 'search', 'mark'], help="Operation to perform")
+    parser.add_argument('operation', choices=['add', 'remove', 'update', 'list', 'search', 'mark'], help="Operation to perform")
+    parser.add_argument('--name', type=str, help="Place name")
+    parser.add_argument('--location', type=str, help="Place location")
+    parser.add_argument('--description', type=str, help="Place description")
+    parser.add_argument('--category', type=str, help="Place category")
+    parser.add_argument('--visited', type=bool, help="Visited status")
+    parser.add_argument('--search_key', type=str, help="Search key")
+    parser.add_argument('--search_value', type=str, help="Search value")
+    
+    args = parser.parse_args()
+    manager = PlacesToVisit()
+    
